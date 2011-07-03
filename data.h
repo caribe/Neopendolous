c49a4ae48711a7da408468fdbb27b3502062b5fc
caribe <vbuttazzo@yahoo.com>
2011-07-03 12:06:08 +0200

    Move to GPL3

--- data.h
+++ data.h
@@ -1,3 +1,25 @@
+/*
+ * 
+ * Neopedulous - A clone of Pendulous
+ * Copyright (C) 2005 Vincenzo Buttazzo <vbuttazzo@yahoo.com>
+ * 
+ * This file is part of Neopendulous.
+ * 
+ * Neopendulous is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Neopendulous is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 /* Allegro datafile object indexes, produced by grabber v4.0.3, Unix */
 /* Datafile: /home/caribe/pendulous/data.dat */
 /* Date: Tue Mar 15 09:58:06 2005 */
--- editor.c
+++ editor.c
@@ -1,3 +1,25 @@
+/*
+ * 
+ * Neopedulous - A clone of Pendulous
+ * Copyright (C) 2005 Vincenzo Buttazzo <vbuttazzo@yahoo.com>
+ * 
+ * This file is part of Neopendulous.
+ * 
+ * Neopendulous is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Neopendulous is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 #include "editor.h"
 
 #define B_LOAD 1
